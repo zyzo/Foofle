fb092503a662c7b39853173e79959f4ed3bb2041
zyzo <danghaian168@gmail.com>
2015-12-04 18:04:08 +0100

    Inverted index

--- src/FoofleAnalyzer.java
+++ src/FoofleAnalyzer.java
@@ -5,8 +5,12 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class FoofleAnalyzer {
     private TextCleaner cleaner;
@@ -15,17 +19,16 @@ public class FoofleAnalyzer {
         cleaner = new TextCleaner();
     }
 
-    public void processFile(File f) throws IOException {
+    public List<String> processFile(File f) throws IOException {
         Document doc = Jsoup.parse(f, "UTF-8");
         Elements content = doc.getElementsByTag("body");
 
         String s = extractText(content.get(0));
         String[] splitted = s.split("\\s+");
 
-        System.out.println(Arrays.toString(splitted));
         // nettoyer les mots
         List<String> cleanedList = cleaner.clean(Arrays.asList(splitted));
-        System.out.println(cleanedList);
+        return cleanedList;
     }
 
     private String extractText(Element element) {
@@ -42,12 +45,51 @@ public class FoofleAnalyzer {
         return result;
     }
 
+    public class Cell {
+    	String link;
+    	int occur;
+    	@Override
+    	public String toString() {
+    		// TODO Auto-generated method stub
+    		return link + ", " + occur;
+    	}
+    }
+    
+    public void run() throws IOException {
+         File corpusDir = new File("CORPUS");
+         File[] fileList = corpusDir.listFiles();
+         Map<String, List<Cell>> map = new HashMap<>();
+         for (File file : fileList) {
+         	List<String> terms = processFile(file);
+         	for (String term : terms) {
+         		if (map.get(term) == null) map.put(term, new ArrayList<Cell>());
+         		List<Cell> cells = map.get(term);
+         		boolean found = false;
+         		for (Cell cell: cells) {
+         			if (cell.link.equals(file.getName())) {
+         				found = true;
+         				cell.occur += 1;
+         				break;
+         			}
+         		}
+         		if (!found) {
+         			Cell cell = new Cell();
+         			cell.link = file.getName();
+         			cell.occur = 1;
+         			cells.add(cell);
+         		}
+         	}
+         }
+         
+         for (Object c : map.entrySet().toArray()) {
+         	Entry d = (Entry) c;
+         	System.out.println(d.getKey());
+         	System.out.println(Arrays.toString(((List<String>) d.getValue()).toArray()));
+         }
+    }
 
     public static void main(String[] args) throws IOException {
-        FoofleAnalyzer z = new FoofleAnalyzer();
-        
-        File corpusDir = new File("CORPUS");
-        File[] fileList = corpusDir.listFiles();
-        z.processFile(new File("CORPUS/D1.html"));
+    	FoofleAnalyzer z = new FoofleAnalyzer();
+    	z.run();
     }
 }
